b004f393120237e93225af7c95b094629ee3b629
zxhuen <zxhuen324@gmail.com>
2026-07-20 21:36:51 +0800

    db migration

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from alembic import context
 
 from app.core.database import Base
 from app.core.config import settings
-from app.models import Person
+import app.models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
