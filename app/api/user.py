980366acd5b86c149c1fe5df25d0391be73c1460
zxhuen <zxhuen324@gmail.com>
2026-07-29 14:09:29 +0800

    small changes

--- app/api/user.py
+++ app/api/user.py
@@ -4,8 +4,11 @@ from app.core.database import get_db
 from app.core.security import oauth2_scheme
 from app.services.user_service import get_current_user
 from app.schemas.user import UserResponse
-router = APIRouter(prefix="/User", tags=["User"])
+
 from app.services.user_service import login_user
+
+router = APIRouter(prefix="/User", tags=["User"])
+
 
 @router.get("/get-current-user", response_model=UserResponse)
 async def get_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,14 +1,21 @@
 from pydantic import BaseModel
 
+
 class PremiumType(BaseModel):
     name: str
+    daily_limit: int
 
-class UserResponse(BaseModel):    
+
+class Usage(BaseModel):
+    used_today: int
+
+
+class UserResponse(BaseModel):
     email: str
     display_name: str
     avatar_url: str
     premium_type: PremiumType
+    usage: Usage
 
     class Config:
         from_attributes = True
-
