6d000fd6a551b54dc1d610c4fb54358cd3fa8fa5
zxhuen <zxhuen324@gmail.com>
2026-08-01 14:12:37 +0800

    new fix

--- app/services/flashcards/generate_flashcards.py
+++ app/services/flashcards/generate_flashcards.py
@@ -6,6 +6,6 @@ def generate_answer(prompt: str) -> str:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt,
-        config=types.GenerateContentConfig(temperature=0.3, max_output_tokens=2048),
+        config=types.GenerateContentConfig(temperature=0.3),
     )
     return response.text
